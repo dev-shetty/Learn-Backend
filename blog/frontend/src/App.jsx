9f3609c662b14c6f2df3e9098280e1653b7c6ba8
Deveesh Shetty <deveeshshetty@gmail.com>
2022-11-06 13:19:23 +0530

    Tailwind Installation

--- blog/frontend/src/App.jsx
+++ blog/frontend/src/App.jsx
@@ -1,7 +1,9 @@
-import "./App.css"
-
 function App() {
-  return <div></div>
+  return (
+    <div className="container min-h-screen bg-red-500 flex justify-center items-center">
+      <p className="text-3xl text-white">Hello World</p>
+    </div>
+  )
 }
 
 export default App
