1ec9e80e38b059bdb4f23a179490a69782ad78d2
Deveesh Shetty <deveeshshetty@gmail.com>
2022-11-06 12:33:30 +0530

    Building again using tailwind

--- blog/frontend/src/App.jsx
+++ blog/frontend/src/App.jsx
@@ -1,22 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css"
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import RegisterPage from "./pages/RegisterPage"
 
 function App() {
-  return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/" element={<HomePage />} />
-          {/* TDOD: Add a landing page in / */}
-        </Routes>
-      </Router>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
