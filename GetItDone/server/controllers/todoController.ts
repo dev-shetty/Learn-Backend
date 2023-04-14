39d543b49a3d561dc735741c194bd9d5e98974a9
Deveesh Shetty <deveeshshetty@gmail.com>
2023-04-14 12:26:47 +0530

    ADD: Update and Delete todo routes

--- GetItDone/server/controllers/todoController.ts
+++ GetItDone/server/controllers/todoController.ts
@@ -64,3 +64,67 @@ export const getTodos = async (req: Request, res: Response) => {
     todos,
   })
 }
+
+/*
+ *  @route - PATCH /api/v1/todo/:id
+ *  @desc - Update a specific todo
+ *  @access - Protected
+ */
+export const updateTodo = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const { isCompleted, title, description } = req.body
+  const updates = {
+    isCompleted,
+    title,
+    description,
+  }
+
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    id,
+    { $set: updates },
+    { new: true }
+  )
+
+  if (!updatedTodo) {
+    return res.status(404).json({
+      success: false,
+      message: "Cannot find the requested todo",
+    })
+  }
+
+  return res.status(200).json({
+    success: true,
+    updatedTodo,
+    message: "Todo Updated",
+  })
+}
+
+/*
+ *  @route - DELETE /api/v1/todo/:id
+ *  @desc - Delete the todo
+ *  @access - Protected
+ */
+export const deleteTodo = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+
+    const deletedTodo = await Todo.findByIdAndDelete(id)
+    if (!deletedTodo) {
+      return res.status(404).json({
+        success: false,
+        message: "Cannot find the requested todo",
+      })
+    }
+
+    return res.status(200).json({
+      success: true,
+      deleteTodo,
+      message: "Todo Deleted",
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error,
+    })
+  }
+}
--- GetItDone/server/routes/todoRoutes.ts
+++ GetItDone/server/routes/todoRoutes.ts
@@ -1,10 +1,17 @@
 import express from "express"
-import { createTodos, getTodos } from "../controllers/todoController"
+import {
+  createTodos,
+  deleteTodo,
+  getTodos,
+  updateTodo,
+} from "../controllers/todoController"
 import { authenticate } from "../middleware/authenticate"
 
 const router = express.Router()
 
 router.get("/", authenticate, getTodos)
 router.post("/", authenticate, createTodos)
+router.patch("/:id", authenticate, updateTodo)
+router.delete("/:id", authenticate, deleteTodo)
 
 export default router
